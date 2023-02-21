29f41aabd846948a042872363edf4ee23d4538a4
Alvaro Borrero <ajhades@hotmail.com>
2023-02-21 14:19:21 -0500

    Agregar componente block

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <h2>Ninja Reaction Timer</h2>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/Block.vue';
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() + 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
 };
 </script>
 
